c2f99a9f48d8a5707be8d9e78c3b266c724d3b7c
yunseohhe <dbstj4566@naver.com>
2024-09-05 00:42:58 +0900

    first commit

---
+++ src/main/java/com/sparta/boardflow/BoardFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.boardflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/boardflow/BoardFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.boardflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
